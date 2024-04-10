6b8f66a8eac6277c812f66899ded4e916be9c93f
김용민 <dydals3440@gmail.com>
2024-04-10 21:07:49 +0900

    feat: Site Header Component 제작

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: 'YOLOG',
+  url: 'https://example.com',
+  description: 'Nextjs 14 blog using velite, tailwind and shadcn',
+  author: 'Yongmin Kim',
+  links: {
+    twitter: '',
+    github: 'https://github.com/dydals3440',
+    personalSite: 'https://github.com/dydals3440',
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
