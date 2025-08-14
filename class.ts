134fd85a94a4d67b18cb883f2a8330320cd2c862
김용민 <dydals3440@gmail.com>
2025-08-14 20:06:31 +0900

    docs: HTTP 쿠키 포스팅 (#29)

--- class.ts
+++ class.ts
@@ -1,23 +1,23 @@
 class InvalidAgeError extends Error {
-  constructor(public readonly value: number) {
-    super(`Invalid age: ${value}`);
-    this.name = 'InvalidAgeError';
-  }
+	constructor(public readonly value: number) {
+		super(`Invalid age: ${value}`);
+		this.name = "InvalidAgeError";
+	}
 }
 
 class Person {
-  constructor(private age: number) {
-    if (age < 0) {
-      throw new InvalidAgeError(age);
-    }
-  }
+	constructor(private age: number) {
+		if (age < 0) {
+			throw new InvalidAgeError(age);
+		}
+	}
 }
 
 try {
-  const person = new Person(-1);
-  console.log(person);
+	const person = new Person(-1);
+	console.log(person);
 } catch (error) {
-  if (error instanceof InvalidAgeError) {
-    console.log(error.message); // Invalid age: -1
-  }
+	if (error instanceof InvalidAgeError) {
+		console.log(error.message); // Invalid age: -1
+	}
 }
